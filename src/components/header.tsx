6cdad6485a9da37359d84ac6ffeec0d86ab534c6
ahmedwasm1070 <ahmedwasim1070@gmail.com>
2025-07-03 17:39:50 +0500

    setting up header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,22 @@
 'use client';
 
 export default function Header() {
-	return (<>
-		<div className="bg-white w-full h-10">
+	return (
+		<>
+			<div id="header" className="h-auto py-6 bg-white">
 
-		</div>
-	</>);
+				{/* Left (logo) */}
+				<div id="logo" className="w-auto">
+				</div>
+
+				<nav id="navbar" className="w-auto">
+					<ul className="flex flex-row justify-center items-center text-black gap-x-4">
+						<li>Home</li>
+						<li>About us</li>
+					</ul>
+				</nav>
+
+			</div>
+		</>
+	);
 }
