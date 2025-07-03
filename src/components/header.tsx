e6b25933d4e5d572e7893764667820b1529e0122
ahmedwasm1070 <ahmedwasim1070@gmail.com>
2025-07-03 12:06:40 +0500

    setting up

---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Header() {
+	return (<>
+		<div className="bg-white w-full h-10">
+
+		</div>
+	</>);
+}
---
+++ src/lib/mongod.ts
@@ -0,0 +1,31 @@
+import { MongoClient } from "mongodb";
+
+const uri: string | undefined = process.env.MONGODB_URI;
+const options = {};
+
+if (!uri) {
+	throw new Error(
+		"Please define the MONGODB_URI environment variable inside .env.local",
+	);
+}
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+declare global {
+	// Allow global caching in development
+	var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
+if (process.env.NODE_ENV === "development") {
+	if (!global._mongoClientPromise) {
+		client = new MongoClient(uri, options);
+		global._mongoClientPromise = client.connect();
+	}
+	clientPromise = global._mongoClientPromise!;
+} else {
+	client = new MongoClient(uri, options);
+	clientPromise = client.connect();
+}
+
+export default clientPromise;
